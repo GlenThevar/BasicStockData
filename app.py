477449cfff6b525c90945551b1b91dc04b398afa
Glen Thevar <glenthevar1@gmail.com>
2024-02-19 22:22:47 +0530

    Update app.py CORS

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 from flasgger import Swagger
 import yfinance as yf
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
